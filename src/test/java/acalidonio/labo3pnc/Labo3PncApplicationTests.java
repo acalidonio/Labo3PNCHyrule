677c22a2e4537d7050a72a5f4f8abc97be603822
acalidonio <116671236+acalidonio@users.noreply.github.com>
2026-05-23 16:00:10 -0600

    Feat: añadir archivos de proyecto

---
+++ src/test/java/acalidonio/labo3pnc/Labo3PncApplicationTests.java
@@ -0,0 +1,13 @@
+package acalidonio.labo3pnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo3PncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
