1e7fb49f83616f675667e66560819cfa02a44128
acalidonio <116671236+acalidonio@users.noreply.github.com>
2026-05-23 16:02:52 -0600

    Feat: añadir diferentes tipos de respuesta de la api incluida respuesta con paginación

---
+++ src/main/java/acalidonio/labo3pnc/domain/dto/response/SpecimenResponse.java
@@ -0,0 +1,16 @@
+package acalidonio.labo3pnc.domain.dto.response;
+
+import lombok.*;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpecimenResponse {
+    private UUID id;
+    private String name;
+    private String region;
+    private Integer dangerLevel;
+    private Boolean isFriendly;
+}
